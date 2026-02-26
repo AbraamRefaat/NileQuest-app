9b0e43d634ed005b124f07b878ffcdb2ba47984b
AbraamRefaat <abraamrefaat0@gmail.com>
2026-02-26 16:58:45 +0200

    Initialize project: Push NileQuest-app to GitHub (Sanitized)

--- lib/services/places_service.dart
+++ lib/services/places_service.dart
@@ -5,7 +5,7 @@ import '../models/tourist_attraction.dart';
 
 class PlacesService {
   static const String _baseUrl = 'https://maps.googleapis.com/maps/api';
-  static const String _apiKey = 'AIzaSyBCzhyILiS8FlaBqWepcZpUJNTra-ce3Do';
+  static const String _apiKey = 'API_KEY_PLACEHOLDER';
 
   final Dio _dio = Dio();
 
